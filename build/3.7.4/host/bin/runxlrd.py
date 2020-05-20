25d9ba45d6a844dd098426f7b10928a3383e5b3d
bansdude <34757939+bansdude@users.noreply.github.com>
2020-05-20 16:56:07 +0200

    live branch
    first commit

---
+++ build/3.7.4/host/bin/runxlrd.py
@@ -0,0 +1,4 @@
+#!/src/cpython/build/3.7.4/host/bin/python3
+# EASY-INSTALL-SCRIPT: 'xlrd==1.2.0','runxlrd.py'
+__requires__ = 'xlrd==1.2.0'
+__import__('pkg_resources').run_script('xlrd==1.2.0', 'runxlrd.py')
